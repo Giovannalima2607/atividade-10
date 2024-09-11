67cb19a1380842c4ffaaf099f4cfc185fea8b34c
Giovannalima2607 <giovannasilva123@gmail.com>
2024-09-10 23:46:33 -0300

    digite um numero para saber se e par ou impar

---
+++ atividade10.py
@@ -0,0 +1,8 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+#receber um numero inteiro
+numero = float(input('digite um numero para saber se é par ou impar:'))
+resto = numero % 2
+if resto == 0:
+    print('numero e par')
+    else:
+        print('numero e par')
